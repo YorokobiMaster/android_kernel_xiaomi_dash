14b74db104aff0763fb138434622e72590ed93db
YorokobiMaster <50650009+YorokobiMaster@users.noreply.github.com>
2026-08-19 06:55:00 +0800

    mtd: defer built-in block2mtd retry

--- drivers/mtd/devices/block2mtd.c
+++ drivers/mtd/devices/block2mtd.c
@@ -26,6 +26,7 @@
 #include <linux/list.h>
 #include <linux/init.h>
 #include <linux/mtd/mtd.h>
+#include <linux/kthread.h>
 #include <linux/mutex.h>
 #include <linux/mount.h>
 #include <linux/slab.h>
@@ -237,9 +238,10 @@ static struct block_device __ref *mdtblock_early_get_bdev(const char *devname,
 		blk_mode_t mode, int timeout, struct block2mtd_dev *dev)
 {
 	struct block_device *bdev = ERR_PTR(-ENODEV);
-	int i;
 
 #ifdef MODULE
+	int i;
+
 	for (i = 0; i <= timeout; i++) {
 		if (i)
 			msleep(1000);
@@ -255,27 +257,13 @@ static struct block_device __ref *mdtblock_early_get_bdev(const char *devname,
 	if (system_state >= SYSTEM_RUNNING)
 		return bdev;
 
-	/*
-	 * We might not have the root device mounted at this point.
-	 * Try to resolve the device name by other means.
-	 */
-	for (i = 0; i <= timeout; i++) {
+	/* Ramdisk-backed storage drivers cannot load while an initcall waits. */
+	{
 		dev_t devt;
 
-		if (i)
-			/*
-			 * Calling wait_for_device_probe in the first loop
-			 * was not enough, sleep for a bit in subsequent
-			 * go-arounds.
-			 */
-			msleep(1000);
 		wait_for_device_probe();
-
-		if (!early_lookup_bdev(devname, &devt)) {
+		if (!early_lookup_bdev(devname, &devt))
 			bdev = blkdev_get_by_dev(devt, mode, dev, NULL);
-			if (!IS_ERR(bdev))
-				break;
-		}
 	}
 #endif
 	return bdev;
@@ -511,13 +499,43 @@ static int block2mtd_setup(const char *val, const struct kernel_param *kp)
 module_param_call(block2mtd, block2mtd_setup, NULL, NULL, 0200);
 MODULE_PARM_DESC(block2mtd, "Device to use. \"block2mtd=<dev>[,[<erasesize>][,<label>]]\"");
 
+#ifndef MODULE
+static struct task_struct *block2mtd_retry_task;
+
+static int block2mtd_retry_thread(void *unused)
+{
+	int attempts = MTD_DEFAULT_TIMEOUT * 4;
+	int i;
+
+	for (i = 0; i <= attempts && !kthread_should_stop(); i++) {
+		if (i)
+			msleep(250);
+		block2mtd_setup2(block2mtd_paramline);
+		if (!list_empty(&blkmtd_device_list)) {
+			pr_info("async attach succeeded after %d attempt(s)\n", i + 1);
+			return 0;
+		}
+	}
+
+	pr_err("async attach timed out after %d ms\n",
+	       MTD_DEFAULT_TIMEOUT * 1000);
+	return 0;
+}
+#endif
+
 static int __init block2mtd_init(void)
 {
 	int ret = 0;
 
 #ifndef MODULE
-	if (strlen(block2mtd_paramline))
-		ret = block2mtd_setup2(block2mtd_paramline);
+	if (strlen(block2mtd_paramline)) {
+		block2mtd_retry_task = kthread_run(block2mtd_retry_thread, NULL,
+						 "block2mtd_retry");
+		if (IS_ERR(block2mtd_retry_task)) {
+			ret = PTR_ERR(block2mtd_retry_task);
+			block2mtd_retry_task = NULL;
+		}
+	}
 	block2mtd_init_called = 1;
 #endif
 
@@ -528,6 +546,11 @@ static int __init block2mtd_init(void)
 static void block2mtd_exit(void)
 {
 	struct list_head *pos, *next;
+
+#ifndef MODULE
+	if (block2mtd_retry_task)
+		kthread_stop(block2mtd_retry_task);
+#endif
 
 	/* Remove the MTD devices */
 	list_for_each_safe(pos, next, &blkmtd_device_list) {
